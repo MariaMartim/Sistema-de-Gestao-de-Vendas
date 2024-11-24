0c4060ebd46036caf3ca61314a4d2759e74d1019
Maria Martim <mariaedu.martim@gmail.com>
2024-11-24 19:06:54 -0300

    organização

--- database/db_config.py
+++ database/db_config.py
@@ -1,9 +1,15 @@
+from dotenv import load_dotenv
+import os
 import mysql.connector
+
+load_dotenv()
 
 # Connect to the database
 connection = mysql.connector.connect(
     user= 'root',
     password= '1703Lunna@',
     host= '127.0.0.1',
-    database= 'LojaRoupas',
+    database= 'LojadeRoupas',
 )
+
+print(connection) # <mysql.connector.connection.MySQLConnection object at 0x7f8b3c1b3d30>
