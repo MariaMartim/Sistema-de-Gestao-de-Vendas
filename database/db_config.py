0907b236c00bb001ca208f247f81b08b92bf024c
Maria Martim <mariaedu.martim@gmail.com>
2025-03-06 19:44:31 -0300

    add .env

--- database/db_config.py
+++ database/db_config.py
@@ -1,12 +1,16 @@
 # Description: This file is responsible for the database connection configuration.
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Connect to the database
 connection = mysql.connector.connect(
-    user= 'root',
-    password= '1703Lunna@',
-    host= '127.0.0.1',
-    database= 'LojadeRoupas',
+    user= os.getenv("USER"),
+    password= os.getenv("PASSWORD"),
+    host= os.getenv("HOST"),
+    database= os.getenv("DATABASE"),
 )
 
 database_url = "mysql+mysqlconnector://root:1703Lunna%40@127.0.0.1/LojaRoupas"
